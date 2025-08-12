ce481e421e187184e43adbf51519019736407200
alisaooov <alisapluss@mail.ru>
2025-08-12 22:14:13 +0500

    added apdate animation

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-// const h1=document.querySelector("h1")
-// h1.innerHTML=h1.textContent.split("").map(letter=>`<span>${letter}</span>`).join("")
+const h1=document.querySelector("h1")
+h1.innerHTML=h1.textContent.split("").map(letter=>`<span>${letter}</span>`).join("")
 
-// anime({
-//   targets:".name span",
-//   translateY:[
-//     {value:-40, duration:500,easing:"easeOutExpo"},
-//     {value:0,duration:800,easing:"easeOutSine", delay:100}
-//   ],
-//   rotate:"1turn",
-//   delay:anime.stagger(1),
-//   loop:true
-// })
+anime({
+  targets:".name span",
+  translateY:[
+    {value:-50, duration:500,easing:"easeOutExpo"},
+    {value:0,duration:800,easing:"easeOutBounce", delay:100}
+  ],
+  rotate:"1turn",
+  delay:anime.stagger(50),
+  loop:true
+})
 
 
